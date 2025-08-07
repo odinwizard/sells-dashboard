3f6031b2ed3dca929e8c3966b1b5358e2b1f4735
Anindya Manna <anindyamanna2013@gmail.com>
2025-08-07 18:39:37 +0530

    Add all the routers for sales route.

---
+++ server/routes/sales.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/"});
+const {getFilteredSales, getProductDistribution, getRegionDistribution, getSalesSummery, getSalesTrend} = require("../controllers/sales.controller");
+const {uploadFile} = require("../controllers/upload.controller");
+
+router.post("/upload", upload.single('file'), uploadFile);
+
+router.get("/summery", getSalesSummery);
+router.get("/sales", getFilteredSales);
+router.get("/trend", getSalesTrend);
+router.get("/products", getProductDistribution);
+router.get("/regions", getRegionDistribution);
+
+
